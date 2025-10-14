d3a889d2cb9390af6a5b284c665104b8242cbf57
ESTUDIO <ESTUDIO@DESKTOP-C63DQC4>
2025-10-14 18:29:34 -0300

    probando

---
+++ src/ejercicioppt/Producto.java
@@ -0,0 +1,37 @@
+package ejercicioppt;
+
+public class Producto implements Comparable<Producto>{
+    private String nombre;
+    private double precio;
+    
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " $" + precio;
+    }
+    
+    @Override
+    public int compareTo(Producto otro){
+        return Double.compare(this.precio, otro.precio);
+    }
+}
